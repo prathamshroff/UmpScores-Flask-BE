41959862096a4233d62269e39ec47d3306e7b72a
Curtis Mason <cumason@bu.edu>
2019-07-15 04:35:54 -0400

    Added swagger support

--- app.py
+++ app.py
@@ -4,13 +4,20 @@ import sys, os
 import boto3
 import time
 from boto3.dynamodb.conditions import Key, Attr
+from flask_cors import CORS
+from flask import Flask, request
+from flask_restplus import Resource, Api, reqparse, fields
+from models import UmpireModel, GameModel
 sys.path.append('./src')
 from Datasets import Dataset
 from CloudSearch import Search
+
 with open('.config.json') as f:
-	configs = json.load(f)
+    configs = json.load(f)
 
 app = Flask(__name__)
+api = Api(app, default ="Umpires and Games")
+CORS(app)
 app.config["DEBUG"] = True
 
 # Custom boto3 wrappers. Will handle data sanitization and malicious queries in the future
@@ -19,143 +26,101 @@ games_dataset = Dataset(configs['iam-user'], 'Refrating-Games')
 umpires_text_search = Search(configs['iam-user'], configs['cloud-search']['umpires-url'])
 games_text_search = Search(configs['iam-user'], configs['cloud-search']['games-url'])
 
-# TODO Remove this ('/') endpoint for future versions
-@app.route('/', methods=['GET'])
-def getUmpire():
-	"""
-	Test URL to ensure dynamodb connection is stable
+umpire_model = api.model('Umpires', UmpireModel)
+game_model = api.model('Games', GameModel)
+search_api_object = api.model('SearchAPIObject', 
+    {
+        'umpire-search-results': fields.List(fields.Nested(umpire_model)),
+        'game-search-results': fields.List(fields.Nested(game_model))
+    }
+)
 
-	Response
-	----------
-	If this endpoint is working you should get this exact json object:
-	{
-		"ump": "Bill Welke",
-		...
-	}
-	""" 
-	if request.method == 'GET':
-		data = json.dumps(umpires_dataset.get({"ump":"Bill Welke", "number": 3}), use_decimal=True)
-		resp = Response(data, status=200, mimetype='application/json')
-		return resp
+search_parser = api.parser()
+search_parser.add_argument('q', type=str, help=
+    '''query string which will find relevant Umpire, and Game data
+        
+    ?q="jordan baker"''', required=True)
 
-@app.route('/search', methods=['GET'])
-def search():
-	"""
-	Search URL which takes a string in the form of a get request and
-	compares that query against a dynamodb table by interfacing through CloudSearch
 
-	Get
-	----------
-	search : string
-		query string which will be sent to aws cloudsearch to find relevant search
-		results.
-			e.g. ?search="jordan baker"
+@api.route('/search')
+class QuerySearch(Resource):
+    @api.doc(parser=search_parser)
+    @api.response(200, 'OK', search_api_object)
+    def get(self):
+        """
+        Search query against our database and get relevant data
+        
+        Description
+        ----------
+        Takes in some arbitrary query string such as 'Jordan Baker' or '1984 all stars'
+        which we use to find relevant search results within both our games and umpire databases.
+        Unfourtunately, games aren't quite functional yet so only umpire search results will be relevant
+        for the time being. Return data will be a dictionary with two keys, 'umpire-search-results' 
+        which contains an array of Umpire objects, and 'game-search-results' which is an array of
+        Game objects. Both arrays are sorted with respect to relevancy with more relevant searches 
+        being at smaller indices.
+        """
+        query = request.args.get('q')
+        data = {
+            'umpire-search-results': umpires_text_search.get(query), 
+            'game-search-results': games_text_search.get(query)
+        }
+        data = json.dumps(data, use_decimal=True)
+        resp = Response(data, status=200, mimetype='application/json')
+        return resp
 
-	Response
-	----------
-	{"umpire-search-results": [{...}, {...}, ...], "game-search-results": [{...}, {...}, ...]}
-		The "umpires" value and the "games" value are similarly formatted such that
-		each array contains several dictionaries representing relevant search results. Left most
-		indices contain more relevant search results. Each dictionary would be an entire data entry 
-		from their respective table. For example, a dictionary under the umpires key
-		would represent all of the statistics about some arbitrary umpire while in 
-		games one dictionary would represent all the statistics about some game. Finally,
-		every dictionary has a _score key with a decimal value representing how relevant
-		that score is. As stated before, left most indices will have higher scores because
-		they are more relevant.
-			e.g. 
-			{
-				"umpire-search-results": 
-					[
-						{
-							"ump": "Bill Welke", 
-							...
-							"_score": "0.7546"
-						}, 
-						...
-					], 
-				"game-search-results": [...]
-			}
-	"""
-	if request.method == 'GET':
-		query = request.args.get('search')
-		data = {
-			'umpire-search-results': umpires_text_search.get(query), 
-			'game-search-results': games_text_search.get(query)
-		}
-		data = json.dumps(data, use_decimal=True)
-		resp = Response(data, status=200, mimetype='application/json')
-		return resp
+@api.route('/get-all-umps')
+class GetAllUmps(Resource):
+    @api.marshal_list_with(umpire_model)
+    def get(self):
+        """
+        Returns every umpire within the Umpires dynamodb table
 
-@app.route('/get-all-umps')
-def getAllUmps():
-	"""
-	Returns every umpire within the Umpires dynamodb table
-
-	Response
-	----------
-	array of dictionaries
-		Every dictionary within this array contains all of the statistics
-		regarding a singular umpire. The response will resemble the following:
-			e.g. 
-			[
-				{
-					"Umpires": "jordan baker"
-					"attr1": "val1",
-					"attr2": "val2",
-					...
-				}, ...
-			]
-	"""
-	data = json.dumps(umpires_dataset.scan(), use_decimal=True)
-	resp = Response(data, status=200, mimetype='application/json')
-	return resp
+        Description
+        ----------
+        Will return a list of every Umpire object we own. Used for the front end to periodically cache 
+        Umpires. Ideally, this dataset will be updated/changed at some set periodic interval to include
+        recent game statistics.
+        """
+        data = json.dumps({'items': umpires_dataset.scan()}, use_decimal=True)
+        resp = Response(data, status=200, mimetype='application/json')
+        return resp
 
 # TODO handle start and end times differently with actual time objects 
 # instead of just integers
-@app.route('/get-games', methods=['GET'])
-def getGames():
-	"""
-	Returns all the data from every game within some given time frame
+get_games_parser = api.parser()
+get_games_parser.add_argument('start', type=int, help='Starting point for the timeframe', required=True)
+get_games_parser.add_argument('end', type=int, help='Ending point for the timeframe', required=True)
+@api.deprecated
+@api.route('/get-games', methods=['GET'])
+class GetGames(Resource):
+    @api.marshal_list_with(game_model)
+    @api.doc(parser=get_games_parser)
+    def get(self):
+        """
+        Returns every Game object within our database.
 
-	Get
-	----------
-	start : integer
-		beginning pointer for the time frame of which we will select games from
-	end : integer
-		ending pointer for the time frame of which we will select games from
+        Description
+        ----------
+        Will return every game object within some timeframe. Is currently deprecated because
+        game data is currently mock info and not usable.
+       
+        """
+        if request.method == 'GET':
+            try:
+                start = int(request.args.get('start'))
+                end = int(request.args.get('end'))
+            except ValueError as e:
+                return 'Please give start and end number fields', 200
 
-	Response
-	----------
-	array of dictionaries
-		Every dictionary within this array contains all of the statistics
-		regarding a singular game. Every game that occurred within the start
-		and end parameters timeframe will be selected for this array.
-		The response will resemble the following:
-			e.g. 
-			[
-				{
-					"Games": "Game1"
-					"Umpire": "ryan additon",
-					...
-				}, ...
-			]
-	"""
-	if request.method == 'GET':
-		try:
-			start = int(request.args.get('start'))
-			end = int(request.args.get('end'))
-		except ValueError as e:
-			return 'Please give start and end number fields', 200
-
-		filterExpression = Attr('timeStamp').between(start, end)
-		data = json.dumps(
-			games_dataset.scan(FilterExpression=filterExpression), use_decimal=True
-		)
-		resp = Response(data, status=200, mimetype='application/json')
-		return resp
+            filterExpression = Attr('timeStamp').between(start, end)
+            data = json.dumps(
+                {'items': games_dataset.scan(FilterExpression=filterExpression)}, use_decimal=True
+            )
+            resp = Response(data, status=200, mimetype='application/json')
+            return resp
 
 if __name__ == '__main__':
-	# getUmpires()
-	# app.run('0.0.0.0', port=80)
-	app.run('127.0.0.1', port=3000)
+    # getUmpires()
+    # app.run('0.0.0.0', port=80)
+    app.run('127.0.0.1', port=3000)
