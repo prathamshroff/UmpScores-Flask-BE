1458d56d24cc428c2a9324811023afea13190ab1
Curtis Mason <cumason@bu.edu>
2019-08-27 01:34:37 -0400

    Adjusted swagger documentation

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ games_text_search = Search(configs['iam-user'], configs['cloudsearch']['games'][
 umpires_dataset = Table(configs['iam-user'], 'refrating-team-stats-v1', umpires_text_search)
 games_dataset = Table(configs['iam-user'], 'refrating-game-stats-v1', games_text_search)
 umpire_id_lookup = Table(configs['iam-user'], 'refrating-umps-lookup')
-ALL_UMPIRE_DATA = umpires_dataset.scan()
 games_date_lookup = Table(configs['iam-user'], 'refrating-games-lookup')
+ALL_UMPIRE_DATA = umpires_dataset.scan()
 
 
 # Setup flask cors and swagger
@@ -76,17 +76,10 @@ class QuerySearch(Resource):
         
         Description
         ----------
-        Takes in some arbitrary query string such as 'Jordan Baker' or '1984 all stars'
-        which we use to find relevant search results within both our games and umpire databases.
-        Unfourtunately, games aren't quite functional yet so only umpire search results will be relevant
-        for the time being. Return data will be a dictionary with two keys, 'umpire-search-results' 
-        which contains an array of Umpire objects, and 'game-search-results' which is an array of
-        Game objects. Both arrays are sorted with respect to relevancy with more relevant searches 
-        being at smaller indices.
+        Takes in some arbitrary query string such as 'Jordan Baker'
+        and returns relevant umpire name results with their respective profile picture.
         """
         query = request.args.get('q')
-
-        # print(umpires_text_search.get(query))
         resp = umpires_text_search.get(query)
         for obj in resp:
             obj.update({'ump_profile_pic': 'https://{0}.s3.amazonaws.com/umpires/{1}+{2}'.format(
@@ -100,12 +93,13 @@ class QuerySearch(Resource):
         resp = Response(data, status=200, mimetype='application/json')
         return resp
 
+
 @api.route('/get-all-ump-ids')
 class GetAllUmps(Resource):
     @api.response(200, 'OK', get_all_umpire_id_pairs)
     def get(self):
         """
-        Returns the names and our unique identifiers for every umpire within our dataset
+        Returns the names with our unique identifiers for every umpire within our dataset
 
         Description
         ----------
@@ -118,22 +112,22 @@ class GetAllUmps(Resource):
         return resp
 
 
-
 @api.route('/get-all-umps')
 class GetAllUmps(Resource):
     @api.response(200, 'OK', get_all_umps_model)
     def get(self):
         """
-        Returns the names and our unique identifiers for every umpire within our dataset
+        Returns every umpire record we have
 
         Description
         ----------
-        Will return a list of all umpire names and id's. Can be used as a quick hash map
-        to convert id's into names and vice versa, or to simply have a list of all umpire names
+        Will return a roughly 10mb object containing every record for every umpire
+        that we have cached. 
         """
         data = json.dumps({'umpires': ALL_UMPIRE_DATA}, use_decimal=True)
         resp = Response(data, status=200, mimetype='application/json')
         return resp
+
 
 get_games_parser = api.parser()
 get_games_parser.add_argument('start', type=str, help='20xx-xx-xx', required=True)
@@ -144,13 +138,11 @@ class GetGames(Resource):
     @api.doc(parser=get_games_parser)
     def get(self):
         """
-        Returns every Game object within our database.
+        Returns every Game object within our database within some given time frame.
 
         Description
         ----------
-        Will return every game object within some timeframe. Is currently deprecated because
-        game data is currently mock info and not usable.
-       
+        Will return every game object within some timeframe.
         """
         if request.method == 'GET':
             try:
@@ -170,6 +162,7 @@ class GetGames(Resource):
             )
             resp = Response(data, status=200, mimetype='application/json')
             return resp
+
 
 if __name__ == '__main__':
     # getUmpires()
